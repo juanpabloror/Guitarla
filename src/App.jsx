1b0bba9c33d1bbb87de5de09813d1f61d5ce94c9
Juan Pablo Roman <xdylol2002@hotmail.com>
2026-06-12 22:28:57 -0600

    feat: implement custom useCart hook for cart management and refactor App and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,36 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Guitar from './components/Guitar'
-import { db } from './data/db' 
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { useCart } from './hooks/useCart'
 
 function App() {
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
 
-  const MAX_ITEMS = 5
-  const MIN_ITEM = 1
-
-  function addToCart(item){
-    const exists = cart.findIndex(guitar => guitar.id === item.id)
-    if(exists === -1){
-      item.quantity = 1
-      console.log('Agregando arreglo...');
-      setCart([item, ...cart])
-
-    }else{
-      console.log('Agregando elemento copiado...');
-      const updatedCart = [...cart];
-      updatedCart[exists].quantity++;
-      setCart(updatedCart)
-    }
-  }
-
-
-  function removeFromCart (id){
-    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id) )
-  }
-
-  function increaseQuantity(id){
-    const updatedCart = cart.map( item => {
-      if (item.id === id && item.quantity < MAX_ITEMS){
-        return{
-          ...item,
-          quantity: item.quantity + 1
-        }
-      }
-      return item
-    })
-
-    setCart(updatedCart)
-  }
-
-  function clearCart(){
-    setCart([])
-  }
-
-  function decreaseQuantity(id) {
-    const updatedCart = cart.map(item => {
-      if(item.id === id && item.quantity !== MIN_ITEM){
-        return{
-          ...item,
-          quantity: item.quantity - 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
+  const {data, addToCart, cart, 
+    removeFromCart, decreaseQuantity, increaseQuantity, 
+    clearCart, cartTotal, isEmpty} = useCart()
   return (
     <>
     <Header 
-    cart={cart} 
-    removeFromCart={removeFromCart} 
-    decreaseQuantity={decreaseQuantity} 
-    increaseQuantity={increaseQuantity}
-    clearCart = {clearCart}
+      cart={cart}
+      removeFromCart={removeFromCart}
+      decreaseQuantity={decreaseQuantity}
+      increaseQuantity={increaseQuantity}
+      clearCart={clearCart}
+      isEmpty={isEmpty}
+      cartTotal={cartTotal}
     />
+    
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map((guitar) => (
-            <Guitar
-              key={guitar.id}
-              guitar={guitar}
-              cart = {cart}
-              setCart={setCart}
-              addToCart = {addToCart}
-              
-            /> 
-          ))}
-           
+            {data.map((guitar) => (
+                <Guitar 
+                  key={guitar.id}
+                  guitar={guitar}
+                  addToCart={addToCart}
+                />
+            ))}
+            
         </div>
     </main>
 
@@ -94,7 +40,6 @@ function App() {
             <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
     </footer>
-
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart}) {
+export default function Header({ 
+    cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart,
+    cartTotal, isEmpty
+}) {
 
-    const isEmpty = () => cart.length === 0
-    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
+    
     return (
         <header className="py-5 header">
             <div className="container-xl">
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div
                             className="carrito"
                         >
-                            <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                {isEmpty() ? (
+                                {isEmpty ? (
                                     <p className="text-center">
                                         El carrito esta vacio
                                     </p>
@@ -78,7 +80,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                                                 ))}
                                             </tbody>
                                         </table>
-                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal() }</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                         <button className="btn btn-dark w-100 mt-3 p-2" onClick={clearCart}>Vaciar Carrito</button>
                                     </>
                                 )}
